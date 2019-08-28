5b1bd5a3ba45377e08b8670384e0816011cd1abc
AJ Grande <ajgrande924@gmail.com>
2019-08-27 19:17:35 -0700

    Add terraform vpc examples - wip

---
+++ terraform/vpc_ec2_ebs/vars.tf
@@ -0,0 +1,19 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-0cfee17793b08a293"
+    us-west-1 = "ami-09eb5e8a83c7aa890"
+    us-east-2 = "ami-0f93b5fd8f220e428"
+    us-west-2 = "ami-0b37e9efc396e4c38"
+  }
+}
--- vagrant/README.md
+++ vagrant/README.md
@@ -17,3 +17,14 @@ vagrant box remove ubuntu/xenial64
   - `awscli`
   - `terraform@0.11.7`
   - `packer@1.2.4`
+
+### misc
+
+```sh
+# add vagrant scp plugin
+vagrant plugin install vagrant-scp
+
+# vagrant scp example
+# vagrant scp <file> <box_id>:<target_location>
+vagrant scp instance.tr 29c2765:/home/vagrant
+```
